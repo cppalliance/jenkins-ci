f51936c888fe2423a64528a743311a4802b5fafd
sdarwin <samuel.d.darwin@gmail.com>
2026-03-26 08:07:41 +0100

    Debug antora using decimal

--- scripts/boostorg_decimal_prebuild.sh
+++ scripts/boostorg_decimal_prebuild.sh
@@ -3,3 +3,5 @@
 set -xe
 echo "export PRTEST=prtest3" >> jenkinsjobinfo.sh
 echo "export ONLY_BUILD_ON_DOCS_MODIFICATION=true" >> jenkinsjobinfo.sh
+echo "export ANTORA_LOG_LEVEL=debug" >> jenkinsjobinfo.sh
+
