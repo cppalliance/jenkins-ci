4a01c619f447eab006f5eab81f75d0bf9a604bd3
sdarwin <samuel.d.darwin@gmail.com>
2026-09-07 16:17:00 -0600

    Update nodejs to 22.23.2

--- scripts/boostorg_boostbook_build.sh
+++ scripts/boostorg_boostbook_build.sh
@@ -20,7 +20,7 @@ else
     echo "Use home dir nvm installation"
 fi
 mkdir -p "$NVM_DIR"
-export NODE_VERSION=18.18.1
+export NODE_VERSION=22.23.2
 # The container has a pre-installed nodejs. Overwrite those again.
 export NVM_BIN="$NVM_DIR/versions/node/v${NODE_VERSION}/bin"
 export NVM_INC=$NVM_DIR/versions/node/v${NODE_VERSION}/include/node
